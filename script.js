39c7722157bf6dbd5c85db1a7e51d1b85f8d639f
puczilka <marta.monika.kwiatkowska@gmail.com>
2019-12-01 14:31:14 +0000

    new-version

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
    $('#nav').localScroll({duration:800});
 });
+
+
+// // When the user scrolls the page, execute myFunction
+// window.onscroll = function() {myFunction()};
+//
+// // Get the navbar
+// var navbar = document.getElementById("nav");
+//
+// // Get the offset position of the navbar
+// var sticky = navbar.offsetTop;
+//
+// // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+function addMargin() {
+    window.scrollTo(0, window.pageYOffset - 180);
+}
+
+window.addEventListener('hashchange', addMargin);
